864da676243382267bb9527203acf79030d31efb
Behnam Tehrani <behnam777@gmail.com>
2025-05-05 11:21:52 -0400

    configuration

--- custom-application-config.mjs
+++ custom-application-config.mjs
@@ -5,15 +5,15 @@ import { PERMISSIONS, entryPointUriPath } from './src/constants';
  */
 const config = {
   name: 'sellertools',
-  entryPointUriPath,
-  cloudIdentifier: 'gcp-us',
+  entryPointUriPath: '${env:ENTRY_POINT_URI_PATH}',
+  cloudIdentifier: '${env:CLOUD_IDENTIFIER}',
   env: {
-    development: {
-      initialProjectKey: 'us-store',
-    },
     production: {
-      applicationId: 'TODO',
-      url: 'https://your_app_hostname.com',
+      applicationId: '${env:CUSTOM_APPLICATION_ID}',
+      url: '${env:APPLICATION_URL}',
+    },
+    development: {
+      initialProjectKey: '${env:INITIAL_PROJECT_KEY}',
     },
   },
   oAuthScopes: {
