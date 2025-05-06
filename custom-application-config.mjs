34f6344f088c3fd6faa7a9965c14460c56669e8e
Behnam Tehrani <15884572+behnamt@users.noreply.github.com>
2025-05-06 17:07:18 -0400

    CMS (#6)

--- custom-application-config.mjs
+++ custom-application-config.mjs
@@ -16,6 +16,9 @@ const config = {
       initialProjectKey: '${env:INITIAL_PROJECT_KEY}',
     },
   },
+  additionalEnv: {
+    CMS_API_URL: '${env:CMS_API_URL}',
+  },
   oAuthScopes: {
     view: [
       'view_products', 
@@ -33,6 +36,12 @@ const config = {
       'manage_product_selections',
       'manage_cart_discounts'
     ],
+  },
+  headers:{
+    csp: {
+      'script-src': ['*.commercetools.app'],
+      "connect-src": ['*.commercetools.app'],
+    }
   },
   icon: '${path:@commercetools-frontend/assets/application-icons/rocket.svg}',
   mainMenuLink: {
--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -1,13 +1,46 @@
+import Spacings from '@commercetools-uikit/spacings';
 import React from 'react'
-
+import styles from './content.module.css';
+import Text from '@commercetools-uikit/text';
+import PrimaryButton from '@commercetools-uikit/primary-button';
+import CsmWrapper from './csm-wrapper';
+import { useAuthContext } from '../../contexts/auth-context';
+import { useApplicationContext } from '@commercetools-frontend/application-shell-connectors';
 type Props = {
     onBack: () => void;
     linkToWelcome: string;
 }
 
-const Content = (props: Props) => {
+const Content = ({onBack}: Props) => {
+  const {storeKey} = useAuthContext();
+  const { environment }: { environment: { CMS_API_URL: string } } = useApplicationContext();
   return (
-    <div>Content</div>
+    <Spacings.Stack scale="l">
+        <div className={styles.header}>
+          <div>
+            <Text.Headline as="h1">Contentools</Text.Headline>
+            <Text.Subheadline>
+              Store: <span className={styles.storeKeyHighlight}>{storeKey}</span>
+            </Text.Subheadline>
+          </div>
+          <Spacings.Inline scale="s">
+           
+            <PrimaryButton
+              label="Back to Dashboard"
+              onClick={onBack}
+            />
+          </Spacings.Inline>
+        </div>
+        {storeKey && (
+          <CsmWrapper
+            baseurl={environment.CMS_API_URL}
+            business-unit-key={storeKey}
+            content-item-app-enabled='true'
+            locale='en-US'
+            available-locales='["en-US"]'
+        />
+        )}
+    </Spacings.Stack>
   )
 }
 
